eb08b1087087deb9b74744980f4a07abf24df5df
Darima <49755723+budazhapova@users.noreply.github.com>
2022-08-19 21:12:27 +0200

    added a dataGenerator script to do batch runs per length of formula. sys.exits kept in case of program-breaking bugs being discovered

---
+++ dataGenerator.py
@@ -0,0 +1,4 @@
+from main import main_function
+
+for i in range(200):
+    main_function("generate", 3)
--- main.py
+++ main.py
@@ -37,64 +37,72 @@ def make_model(formula_length, WORKMODE):
     
     return main_world
 
-WORKMODE = "generate"
-# WORKMODE = "load"
 
 
 
 # start setup: first, set variables for the amount of atoms, agents, length of formula to generate
 # and generate it. returns a Model-class object (from here on: world)
-formula_length = randint(3, 15)
-if WORKMODE == "load":
-    formula_length = 7      # CHANGES DEPENDING ON PRESET
-# FIXME: correct max length back to (200?) after testing
-main_world = make_model(formula_length, WORKMODE)
-proof_world = main_world.copy_model()
+def main_function(WORKMODE, formula_length=randint(3, 200)):
+    # formula_length = randint(3, 15)
+    formula_length = 3
+    # FIXME: put randomness back after data gathering
+    if WORKMODE == "load":
+        formula_length = 7      # CHANGES DEPENDING ON PRESET
+    # FIXME: correct max length back to (200?) after testing
+    main_world = make_model(formula_length, WORKMODE)
+    proof_world = main_world.copy_model()
 
-# here the we compare newly generated formula to those already on file
-all_roots = main_world.find_roots(main_world.formula_tree)
-new_root = all_roots[0]
-# calculate depth of the model
-main_world.estimate_depth(new_root)
-# retrieve already recorded formulas from appropriate file
-data_list = retrieve_formulas(formula_length)
-# for every recorded formula, convert back to tree form and compare to the new formula
-if data_list:
-    print("COMPARING TO FILE")
-    for json_obj in data_list:
-        old_formula = json_to_tree(json_obj)
-        formula_match = compare_formulas(old_formula, new_root)
-        print(f"Exact match to new formula: {formula_match}")
-        # if such formula has already been generated, abort
-        if formula_match == True:
-            print(translate_formula(new_root))
-            sys.exit("\nFORMULA ALREADY EXISTS")
+    # here the we compare newly generated formula to those already on file
+    all_roots = main_world.find_roots(main_world.formula_tree)
+    new_root = all_roots[0]
+    # calculate depth of the model
+    main_world.estimate_depth(new_root)
+    # retrieve already recorded formulas from appropriate file
+    data_list = retrieve_formulas(formula_length)
+    # for every recorded formula, convert back to tree form and compare to the new formula
+    if data_list:
+        print("COMPARING TO FILE")
+        for json_obj in data_list:
+            old_formula = json_to_tree(json_obj)
+            formula_match = compare_formulas(old_formula, new_root)
+            print(f"Exact match to new formula: {formula_match}")
+            # if such formula has already been generated, abort
+            if formula_match == True:
+                print(translate_formula(new_root))
+                print("\nFORMULA ALREADY EXISTS")
+                return 0
+                # sys.exit("\nFORMULA ALREADY EXISTS")
 
 
-# start tracking time and RAM usage of the tableau solver
-start_time = time.process_time()
-tracemalloc.start()
+    # start tracking time and RAM usage of the tableau solver
+    start_time = time.process_time()
+    tracemalloc.start()
 
-# check the formula through a tableau and report the results
-# returns whether the formula is a tautology (True or False)
-tautology = tableau_solver(proof_world)
-solving_time = time.process_time() - start_time
-current_memory, peak_memory = tracemalloc.get_traced_memory()
-tracemalloc.stop()
+    # check the formula through a tableau and report the results
+    # returns whether the formula is a tautology (True or False)
+    tautology = tableau_solver(proof_world)
+    solving_time = time.process_time() - start_time
+    current_memory, peak_memory = tracemalloc.get_traced_memory()
+    tracemalloc.stop()
 
-# orig_roots = main_world.find_roots(main_world.formula_tree)
-print("\nTAUTOLOGY = ", tautology)
-print(translate_formula(new_root))
-print(f"Tableau of length {formula_length} and depth {main_world.model_depth} solved in {solving_time} seconds (CPU time) and {peak_memory} bytes peak memory")
+    # orig_roots = main_world.find_roots(main_world.formula_tree)
+    print("\nTAUTOLOGY = ", tautology)
+    print(translate_formula(new_root))
+    print(f"Tableau of length {formula_length} and depth {main_world.model_depth} solved in {solving_time} seconds (CPU time) and {peak_memory} bytes peak memory")
 
-# write generated formula to appropriate file
-write_formulas(formula_length, new_root, data_list)
-# if new formula was proven a tautology, record it in a separate file
-if tautology == True:
-    tautology_list = retrieve_formulas("tautologies")
-    write_formulas("tautologies", new_root, tautology_list)
+    # write generated formula to appropriate file
+    write_formulas(formula_length, new_root, data_list)
+    # if new formula was proven a tautology, record it in a separate file
+    if tautology == True:
+        tautology_list = retrieve_formulas("tautologies")
+        write_formulas("tautologies", new_root, tautology_list)
 
-# TODO: plug in recording of formula + results!
-# ["length", "depth", "tautology", "CPU time (sec)", "memory (bytes)"]
-row = [formula_length, main_world.model_depth, tautology, solving_time, peak_memory]
-record_data_to_csv("testdata", row)
+    # ["length", "depth", "tautology", "CPU time (sec)", "memory (bytes)"]
+    row = [formula_length, main_world.model_depth, tautology, solving_time, peak_memory]
+    record_data_to_csv("testdata", row)
+
+
+WORKMODE = "generate"
+# WORKMODE = "load"
+
+main_function("generate", randint(3,15))
